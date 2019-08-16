169da3406e093b56063c1ed4e17877eee360ddb4
suppachaiwws <suppachaiwws@gmail.com>
2019-08-16 15:57:33 +0700

    asd

--- showBillall.php
+++ showBillall.php
@@ -5,7 +5,7 @@
 <head>
     <style>
         table {
-            
+
             border-collapse: collapse;
             width: 100%;
         }
@@ -27,48 +27,76 @@
     <?php include "./component/userNav.php"  ?>
 
     <?php
-	ini_set('display_errors', 1);
-	error_reporting(~0);
+    ini_set('display_errors', 1);
+    error_reporting(~0);
 
-   $serverName = "192.168.20.102";
-   $userName = "root";
-   $userPassword = "";
-   $dbName = "db_php_workshop";
+    $serverName = "192.168.20.102";
+    $userName = "root";
+    $userPassword = "";
+    $dbName = "db_php_workshop";
 
-   $conn = mysqli_connect($serverName,$userName,$userPassword,$dbName);
+    $conn = mysqli_connect($serverName, $userName, $userPassword, $dbName);
 
-   $sql = "SELECT * FROM receipt";
+    $sql = "SELECT * FROM receipt";
 
-   $query = mysqli_query($conn,$sql);
+    $query = mysqli_query($conn, $sql);
 
-?>
+    ?>
 
-    <h2>Receipt </h2>
+    <h2>&nbsp;Receipt </h2>
     <hr width=90% size=10 color=#DCDCDC>
 
     <table>
-        
+
         <tr>
-        
-            <th><div align="center">No. </div></th>
-            <th><div align="center">Bill ID</div></th>
-            <th><div align="center">Date</div></th>
-           
+
+            <th>
+                <div align="center">No. </div>
+            </th>
+            <th>
+                <div align="center">Bill ID</div>
+            </th>
+            <th>
+                <div align="center">Date</div>
+            </th>
+            <th>
+                <div align="center">Print</div>
+            </th>
+
         </tr>
-       
+
         <?php
-while($result=mysqli_fetch_array($query,MYSQLI_ASSOC))
-{
-?>
-  <tr>
-    <td><div align="center"><?php echo $result["receipt_id"];?></div></td>
-    <td><div align="center"><?php echo $result["order_id"];?></div></td>
-    <td><div align="center"><?php echo $result["created_at"];?></div></td>
-  </tr>
-<?php
-}
-?>
+        while ($result = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+            ?>
+        <tr>
+            <td>
+                <div align="center"><?php echo $result["receipt_id"]; ?></div>
+            </td>
+            <td>
+                <div align="center"><?php echo $result["order_id"]; ?></div>
+            </td>
+            <td>
+                <div align="center"><?php echo $result["created_at"]; ?></div>
+            </td>
+            
+            <td>
+                <form action="testBill.php" method="post">
+                    <input type="hidden" value=<?php echo $result["receipt_id"]?> name="Bill_id">
+                <div align="center">
+                    <a href="testBill.php">
+                    <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#myModal" name="idOrder" >
+                        Print
+                    </button>
+                    </a>
+                </div>
+                </form>
+                
+            </td>
+        </tr>
+        <?php };?>
     </table>
+       
+        
 </body>
 
 </html>
